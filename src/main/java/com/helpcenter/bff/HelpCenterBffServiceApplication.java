4747afd2716d3f66f5778a05e7bfab53c91466fb
Luis Pérez <luisperez@Luiss-MacBook-Pro.local>
2024-11-13 18:11:45 -0600

    Add main application class

---
+++ src/main/java/com/helpcenter/bff/HelpCenterBffServiceApplication.java
@@ -0,0 +1,12 @@
+package com.helpcenter.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpCenterBffServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpCenterBffServiceApplication.class, args);
+    }
+}
